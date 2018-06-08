80abbceb884416e3f408295f7dbd4ff9ac21a9bb
gmat191@gmail.com <gmat191@gmail.com>
2018-06-08 23:22:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,18 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var c = ["snickers", "hundred grand"]
 c = ["kitkat", ...chocolateBars]
 
-function addElementToBeginningOfArray(a, b){
-  var c = a
-  c.unshift(b)
-  return c
-}
-
-function destructivelyAddElementToBeginningofArray(a, b){
-  a.unshift(b)
-  return a
-}
-function AddElementToEndofArray(x, y){
-  var c = a 
-  c.unshift(b)
-  return c
+function addElementToBeginningOfArray(array, element) {
+  
 }
