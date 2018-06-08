2ac817d8ddc580c052088ae131e35ce79a6aec49
gmat191@gmail.com <gmat191@gmail.com>
2018-06-08 23:22:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-var c = ["snickers", "hundred grand"]
-c = ["kitkat", ...chocolateBars]
+
 
 function addElementToBeginningOfArray(array, element) {
-  return [element...array]
+  return [element,...array]
 }
