da785b28c57b570026fcbe5be2004c875b0b0e57
gmat191@gmail.com <gmat191@gmail.com>
2018-06-08 22:14:14 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var cholocateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningofArray(a, b) {
+  var c = a
+  c.unshift(b)
+  return c
+}
+function destructivelyAddElementToBeginningofArray(a, b) {
+  a.unshift(b)
+  return a
+}
+
+function accessElementinArray(a, b) {
+  
+}
